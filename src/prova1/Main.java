48ccc3661b8e061dc0b684840dc9e3fe7c763417
Alessione <aledanto05@gmail.com>
2025-06-20 09:45:12 +0200

    provajAVA

---
+++ src/prova1/Main.java
@@ -0,0 +1,12 @@
+package prova1;
+
+public class Main {
+    package prova1;
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Ciao da prova1!");
+        }
+    }
+
+}
